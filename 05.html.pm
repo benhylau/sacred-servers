133d2b1c98e66e67985c8e6dbea5ed7ea98253b3
Zach! <webmaster@coolguy.website>
2021-01-15 16:11:06 +1300

    cyberspace (05)

---
+++ 05.html.pm
@@ -0,0 +1,41 @@
+#lang pollen
+
+◊define-meta[title]{
+  Cyberspace
+}
+
+When the web emerged, it was treated as distinct from the real world, like we'd
+discovered a second, imaginary earth. John Perry Barlow published the
+"Declaration of the Independence of Cyberspace", which said so in an official
+decree: "Governments of the Industrial World, you weary giants of flesh and
+steel, I come from Cyberspace, a new home of Mind...You are not welcome among
+us...you have no sovereignty here."
+
+He continued:
+
+"I declare the global social space we are building to be naturally independent
+of the tyrannies you seek to impose on us...Cyberspace does not lie within your
+borders...it is an act of nature that grows through our collective actions. Your
+legal concepts of property, expression, identity, movement, and context do not
+apply to us...They are all based on matter, and there is no matter here."
+
+We came to learn, of course, that this was hooey.
+
+The online world is deeply entangled--inseparable--from our "real" world. The
+cyber world he describes was, from the start, a military project. The things we
+say and do online have a profound effect on our physical lives, and the
+infrastructural needs have a profound effect on the natural world in which
+cyberspace resides.
+
+It is a compelling illusion to declare this a world with no matter, only Mind,
+but it takes a whole lot of matter to maintain the illusion.
+
+For a while it was a useful, semantic distinction for this internet place you
+visited from the world in which you resided. That you were a different person in
+"cyberspace" than in "meatspace". But the distinction makes little sense now,
+cyberspace is more a nostalgic signifier than useful term, and "meatspace" is
+really just for people who like saying "meat" out loud.
+
+There is no separate space. We are all in it together, overlaid and coexisitent.
+And, from my view, to declare a space of ideas and the mind to be necessarily
+separate from the world, is to deny the world the aliveness, the agency, it has.
