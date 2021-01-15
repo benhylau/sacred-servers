b5d53ac6fa189ab095005c6e4414e14849f051a4
Zach! <webmaster@coolguy.website>
2021-01-15 23:03:22 +1300

    add growing online, stop the numbering pattern
    We may wanna move the pieces around later,a dn the numbers get confusing!

---
+++ growing-online.html.pm
@@ -0,0 +1,37 @@
+#lang pollen
+
+◊define-meta[title]{
+  Growing online
+}
+
+At one point, I considered the internet separate from the world, this imaginary
+boundless place. It's likely because, as a kid, my house had dial-up internet,
+and connecting to the internet required a short ritual with arcane sounds. We
+also had one phone line, so the internet could not be in the house continually.
+It was something you visited late at night, like catching a distant AM station
+bouncing down from the sky.
+
+Now, ironically, the internet is something I have to work to get away from. Dig
+deep into phone settings to try to keep it from pestering me about new posts I
+missed, or to quell the habit of checking work chat in the bathroom, stepping
+out of the shower to scroll through text messages with wet hands. My evening
+ritual is to go intentionally offline, shut down the screens and turn them away,
+revisit the simplicity of boredom or a book.
+
+The internet and the web still seem imaginary. We keep up the refrain of "This
+is not real life", while the things said and done online have direct, tangible
+effects in our lives. We see physical media become odd echos of the online
+world, newspaper articles detailing the timeline of a particularly important
+twitter thread, controversies about upcoming films revealed to be a manufactured
+viral campaign. Each day becomes harder to distinguish what's real, whether
+online or off.
+
+And we say it is intangible, yet feel the full physical weight of it. The tics
+of phone checks, the tired eyes and slumped shoulders, the racing heart or
+dopamine rush of a notifying ping. I setup a timer when on the internet now,
+have a buzzer  go off every 25 minutes so I remember to get up and stretch
+and let my eyes focus softly on some unlit thing. It is to help slow this
+imaginary's worlds physical effects on me.
+
+We can feel the effects of the web, even if they're hard to describe or feel too
+obvious to say. What isn't discussed as much is how much we give back to it.
