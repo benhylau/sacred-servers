f1f4b8c2092292adffe8f560ea3327377e1ce693
Zach! <webmaster@coolguy.website>
2021-01-15 23:03:57 +1300

    add prayer to gofundme

---
+++ prayer-to-gofundme.html.pm
@@ -0,0 +1,22 @@
+#lang pollen
+
+◊define-meta[title]{
+Tangible and Intangible
+}
+
+When a gofundme is posted, what all is shared within it? What all imbues it, as
+its signal is boosted and it goes far beyond the original network. When its goal
+is reached, and the possibilities open now in the real-world, is it the same
+feeling as an answered prayer? Or a digital artifact of an answered prayer? What
+feeling lingers in those that don't reach their goals, what energy remains?
+
+When we make a wish to a well, light a candle and speak our hope, what all comes
+along with our voice? What changes when the words are typed instead of said?
+Where do these words go?
+
+In talking of the offline world, the question remains rhetoric, or at least the
+answer is just beyond our reach. In the online world, you can give a simple,
+though, incomplete answer.
+
+It is easy to know where the words go, when typed instead of spoken: they go to
+servers.
