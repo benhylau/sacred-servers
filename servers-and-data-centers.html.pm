ef4c42c1900506c84e6057f263178cffae0b9bc3
Zach! <webmaster@coolguy.website>
2021-01-15 23:29:28 +1300

    add servers and data centers

---
+++ servers-and-data-centers.html.pm
@@ -0,0 +1,30 @@
+#lang pollen
+
+◊define-meta[title]{
+Servers and Data Centers
+}
+
+The spectrum of feelings we share on these handful of websites all end up
+travelling to the same type of place: a server in some distant city owned by
+this same handful.
+
+When you message a friend it does not go to them directly. It is passed as a
+request to a server beyond either of your control, and this server responds to
+your request by, hopefully, passing it along. It will also keep this message
+stored, for as long as it needs or wants to, in a data center owned by this
+handful.
+
+The message is translated to a language unspoken by us, a language
+reduced to an extreme simplicity of 1 an 0, off and on, light and dark, before
+being translated again into energetic charge, recorded into a hard drive's
+platter as a sequence of magnetized momemnts. These tiny, physical, but
+invisible point hold their magnetic charge, this energetic impulse, for as long
+as needed...for in these charged and uncharged points lives your words, ready to
+be recalled again.
+
+What is passed along with everything we post. What all do these data centers
+hold? Can our attachment to them come from something beyond addictive ui and a
+massive budget...but something they'd feel foolish to say aloud? What power have
+we given them, what power do they now hold? How much of what holds those
+pinpoints of a hard drive in place is the words themsleves, adn the feelings we
+passed along with them?
