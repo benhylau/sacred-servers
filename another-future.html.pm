668da9705b802c453926f4d6aa22fcff72f8a946
Zach! <webmaster@coolguy.website>
2021-01-16 00:43:25 +1300

    bring in final essays (final for now)

---
+++ another-future.html.pm
@@ -0,0 +1,30 @@
+#lang pollen
+
+◊define-meta[title]{
+Another Future
+}
+
+What would happen if we gave as much as we do now, all our vulnerable and
+intention-filled requests, but to servers that we held? Or if we passed it
+directly to one another, amplifying the power ourselves?
+
+What would happen if we were upfront with the potential energy, the inarticulate
+sacred power, that can come from voicing our lives and dreams, and storing the
+echoes of this voice. But now it was stored in networks designed to respect,
+amply, and give back to this power?
+
+There is beautiful possibility in decentralized tools because they can be new,
+and local, and small. We can create community networks that grow stronger with
+each new community member, stronger in practical and magical ways, tangible and
+intangible. We can have sun-soaked servers, that can be plentiful and small,
+held in powerful spots of nature not because of what can be extracted, but
+because of what resonates there and what we know can be given back.
+
+We can create tools that are resilient and respectful, respecful of the world
+they exist in, the people giving to them, and the streams that passes through it
+all. We've felt the tremendous weight of power that profane companies hold over
+us. What if we distributed this power among ourselves?
+
+Part of this is poetic metaphor and part of this is me being as literal as I
+can, and it can be hard for me to tell sometimes which is which. But that's
+alright. I like this liminal space of not knowing.
---
+++ sacred-and-profane.html.pm
@@ -0,0 +1,38 @@
+#lang pollen
+
+◊define-meta[title]{
+Sacred and Profane
+}
+
+I will just say my feelings directly: we have given so much energy and intention
+to a handful of centralized platforms, and I think it's become metaphysically
+dangerous.
+
+I don't think it's bad that we share what we do and as much as we do. It's
+intense and beautiful and human. It's the same feelings we've always had, the
+same need to pass them out beyond us we've always had, and we're good at
+adaptation. Liminal spaces is now an instagram account ◊em{and} a creepypasta.
+
+It's dangerous because these platforms use the energy held in their centers, but
+treat it profanely. All our human experience shared at once is reduced to the
+word "content". Our prayers and wishes are flattened alongside everythign else,
+We cannot place them in these spaces intentionally--they are cast to a feed and
+displayed "algorithimically", which is to say mysteriously, nestled between ads
+and banal recaps of television shows. The amount of time you spend reading a
+loved one's post is recorded by the company to adjust your advertising profile,
+so that keywords in your friend's update on their sick dog will be used in some
+future embedded post to sell you a subscription box for soap.
+
+The data center require nature, but show no respect back to it. These centers
+are placed strategically, aware of the power a particular landscape can hold.
+They are giant buildings placed in out-of-sight spaces, but still connected to
+our grid---giant buildings housing billions of humans posts, placed outside
+towns of less than a thousand. Or they are placed in areas where the natural
+landscape can help reduce their operating costs, near rivers that can be
+directed to help power their cooling systems, or in mountains to protect against
+the world-razing war they invariably expect.
+
+There is something simple and pure in all that we share on these sites, and the
+power that this raises, an attachment to each other that is tangible and
+intangible both. And there is something simple and direct in how profanely the
+platforms desecrate it.
