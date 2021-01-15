d9d418515ffbbce8f49d69969bdb51536344229a
Zach! <webmaster@coolguy.website>
2021-01-15 14:47:06 +1300

    first 03

---
+++ 03.html.pm
@@ -0,0 +1,50 @@
+#lang pollen
+
+◊define-meta[Title]{ Time Online }
+
+Today, I don't spend as much time, travelling to sacred wells in distant places.
+I spend a lot of time online. Too much time. It's a place profoundly unquiet, a
+pummeling noise and a rush of information that leaves me feeling both hyped up
+and melancholy. And it's a place that is hard to stop visiting.
+
+It's a bit corny, at this point, to refer to twitter as 'the hell site'; not
+because it isn't true, but because the term is played out. Its hellishness is
+self-evident, and people stopped bringing that up years ago.
+
+Whenever I'm in a conversation about social media, it always seems to take the
+same arc.
+
+"It is terrible, I don't like it."
+
+"And It's making the world worse!"
+
+"I deleted it off my phone, trying to check it intentionally."
+
+"I should fully delete it, honestly."
+
+"Well, I should say, I just use messenger now."
+
+"Of course, it's the only way to talk to some family."
+
+"Right! And, there is some great stuff on there though."
+
+"Terrible stuff, but really funny too."
+
+"And with everything that happened, it was incredible to see the stories coming
+out."
+
+"I have real connections on there, I use it differently, I think. It's the
+groups I'm a part of."
+
+"Honestly, it's really important and some people don't have the privilege to
+give it up."
+
+"Speaking of, Have you seen this?"
+
+"Oh, that's great, have you seen this?"
+
+--
+
+We know it's bad, and still osmething holds us. Something beyond its addictive
+design--like a personal attachment we're hesitant to let go of. What have we
+left clinging to these spaces?
