6a4b232757bcb520bf15bc064a2a226af6a12cd1
Zach! <webmaster@coolguy.website>
2021-01-15 14:36:17 +1300

    bring in part 02

---
+++ 02.html.pm
@@ -0,0 +1,23 @@
+#lang pollen
+
+◊define-meta[title]{
+Power in Place
+}
+
+There are the places and object that feel powerful because I am told they are
+powerful. They are imbued with with belief, tradition, and history. And in these
+places, my body responds appropriately-- a reflexive respect that brings my
+voice to a whisper in a church, finds me taking off my shoes at the entrance of
+a temple, ears pricked up for reverberation in the childhood home of a childhood
+hero.
+
+And there are the places and things that hold power for some small, or
+inarticulate reason. Or, at least, If i were to try to articualte it, I'd feel
+foolish. The one childhood toy I can't let go of, the town I irrationally hate
+stopping at when travelling, when an item seems to "call" to me or when one
+feels strangely "off". It's as if the things are filled with some energy of the
+people before, a loop of time lingering in the space. It's that impulse that
+comes whenever I move to a new home, no matter how bare, clean, and blank the
+new space may be, to clean it all before I move any of our own items in. What
+are we cleaning? What invisible thing needs to be moved to let our own invisible
+thing in?
