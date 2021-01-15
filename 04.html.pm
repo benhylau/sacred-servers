ab43bae1a87257b7b0aea4ff8b2885fdb8ebeb38
Zach! <webmaster@coolguy.website>
2021-01-15 14:59:41 +1300

    Liminal Spaces (04)

---
+++ 04.html.pm
@@ -0,0 +1,16 @@
+#lang pollen
+
+◊define-meta[title]{
+Liminal Spaces
+}
+
+Perhaps a part of the sacred well's power came from being a liminal space. It is
+both land and water; a visible rush from an invisible source. We humans love
+liminal spaces, recognize them as inherently powerful. The rush of emotions that
+come at dusk and dawn, the symbolic depth felt when standing on the beach
+looking out to the ocean. The electrical chill when someone is gone, but hasn't
+yet left.
+
+We are drawn to the inexpressible (or at least I am). The places beyond
+articulation. And what pull there is for that which is not one thing or another,
+but forever hovering in between.
